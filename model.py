0ec5a200b5f43138681b033e284576f8df8acf80
Yaaroslaw <yaaroslaw@gmail.com>
2016-03-28 17:09:25 +0200

    Functions for operations(add/delete).

--- model.py
+++ model.py
@@ -1,33 +1,48 @@
 from random import randint
-season = {'March': [], 'April': [], 'May': []}  # Словник для місяців
-
-class weather_blog(object):
-    def __init__(self, name, days_amount):
-        self.name = name
-        self.days_amount = days_amount
+year = {}
 
 
-
-class day(object):
-    def __init__(self, pressure, temperature, wind):
+class Day(object):
+    def __init__(self, pressure, temperature, wind, month, number):
         self.pressure = pressure
         self.temperature = temperature
         self.wind = wind
+        self.month = month
+        self.number = number
 
 
-def creation_blog():  # Ф-ція для створення місяців в словнику season
-    for k in season.keys():
-        for i in range(5):
-            season[k].append(creation_day())
-    return season
+def add_day(pressure, temperature, wind, month, number):
+    year[month].append(Day(pressure, temperature, wind, month, number))
 
 
-def creation_day():  # Ф-ція для створення днів
-    x = randint(684, 809)
-    y = randint(-5, 20)
-    z = randint(1, 30)
-    day = {"pressure": x, "temperature": y, "wind": z}
-    return day
+def del_day(month, number):
+    for i in year[month]:
+        if i.number == number:
+            year[month].remove(i)
+
+
+def add_month(name):
+    year[name] = []
+
+
+def del_month(name):
+    del year[name]
+
+
+#
+# def creation_blog():  # Ф-ція для створення місяців в словнику season
+#     for k in season.keys():
+#         for i in range(5):
+#             season[k].append(creation_day())
+#     return season
+#
+#
+# def creation_day():  # Ф-ція для створення днів
+#     x = randint(684, 809)
+#     y = randint(-5, 20)
+#     z = randint(1, 30)
+#     day = {"pressure": x, "temperature": y, "wind": z}
+#     return day
 
 
 
